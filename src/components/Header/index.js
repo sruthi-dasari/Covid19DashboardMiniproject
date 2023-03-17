a3e836e2ae8349c21190fcc5d614c983a7e5aa53
sruthi <sruthi@example.com>
2023-03-17 18:18:51 +0000

    in progress

---
+++ src/components/Header/index.js
@@ -0,0 +1,68 @@
+import './index.css'
+
+import Popup from 'reactjs-popup'
+
+import {RiMenuFoldFill} from 'react-icons/ri'
+import {IoMdCloseCircle} from 'react-icons/io'
+
+import {Link} from 'react-router-dom'
+
+import {Component} from 'react'
+
+class Header extends Component {
+  state = {
+    showMenu: false,
+  }
+
+  onClickMenu = () => {
+    this.setState({showMenu: true})
+  }
+
+  onClickClose = () => {
+    this.setState({showMenu: false})
+  }
+
+  render() {
+    const {showMenu} = this.state
+    return (
+      <div className="header-container">
+        <div className="top-header-container">
+          <Link to="/" className="link-item">
+            <h1 className="logo-heading">
+              COVID19<span className="india-text">INDIA</span>
+            </h1>
+          </Link>
+
+          <button
+            type="button"
+            className="menu-button"
+            onClick={this.onClickMenu}
+          >
+            <RiMenuFoldFill className="menu-icon" />
+          </button>
+        </div>
+        {showMenu && (
+          <div className="bottom-header-container">
+            <div>
+              <Link to="/" className="home-and-about-link">
+                Home
+              </Link>
+              <Link to="/about" className="home-and-about-link">
+                About
+              </Link>
+            </div>
+            <button
+              type="button"
+              className="close-button"
+              onClick={this.onClickClose}
+            >
+              <IoMdCloseCircle className="close-icon" />
+            </button>
+          </div>
+        )}
+      </div>
+    )
+  }
+}
+
+export default Header
---
+++ src/components/Home/index.js
@@ -0,0 +1,15 @@
+import './index.css'
+
+import {Component} from 'react'
+
+class Home extends Component {
+  render() {
+    return (
+      <div className="home-container">
+        <p className="text">Hi</p>
+      </div>
+    )
+  }
+}
+
+export default Home
